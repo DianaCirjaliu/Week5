d7c5ad62785a2fe140e05d1a1064eae143834947
Diana Cirjaliu <Diana.Cirjaliu@cognizant.com>
2026-02-10 16:16:50 +0200

    feat(ui):implemented the add to wishlist button and reshaped some of the css from the movie card

--- my-react-app/src/components/MovieCard/MovieCard.jsx
+++ my-react-app/src/components/MovieCard/MovieCard.jsx
@@ -1,16 +1,31 @@
 import "./MovieCard.css";
 import Data from "../../assets/movies/movies.json";
+import FavBtn from "../../components/FavBtn/FavBtn";
 
 function MovieCard() {
   return (
     <div className="container-movies">
       {Data.map((movie) => {
+        let color = "green";
+        const rating = Number(movie.rating);
+
+        if (rating < 5) {
+          color = "red";
+        } else if (rating < 8) {
+          color = "yellow";
+        }
         return (
           <article key={movie.id} className="movie-card">
             <img src={`/images/${movie.image}`} alt={movie.title} />
             <h2>{movie.title}</h2>
             <h3>Genre: {movie.genre}</h3>
-            <h3 >Rating: {movie.rating}</h3>
+            <h3>
+              Rating:{" "}
+              <span className="rating" style={{ color }}>
+                {movie.rating}
+              </span>
+            </h3>
+            <FavBtn></FavBtn>
           </article>
         );
       })}
