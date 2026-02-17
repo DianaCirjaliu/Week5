c9faacb2c8a7e8036b341a67d9e177eb87cbc6a7
Diana Cirjaliu <Diana.Cirjaliu@cognizant.com>
2026-02-17 10:20:15 +0200

    feat(routing)-Task1:each individual movie should be accessible through url directly using the movie individual (ex: movies/344)

--- my-react-app/src/Routes.tsx
+++ my-react-app/src/Routes.tsx
@@ -4,6 +4,8 @@ import HomePage from "./pages/Home/Home";
 import Watchlist from "./pages/Watchlist/Watchlist";
 import { ErrorBoundary } from "react-error-boundary";
 import ErrorFallback from "./components/ErrorFallback/ErorrFallback";
+import MovieDetails from "./components/MovieDetails/MovieDetails";
+import moviesData from "../src/assets/movies/movies.json";
 
 function AppRoutes() {
   return (
@@ -19,6 +21,12 @@ function AppRoutes() {
           <Route path="/home" element={<HomePage />} />
           <Route path="/watchlist" element={<Watchlist />} />
           <Route path="*" element={<div>Page not found (404)</div>} />
+
+          {/*individual page for a movie */}
+          <Route
+            path="/movies/:id"
+            element={<MovieDetails movies={moviesData} />}
+          />
         </Routes>
       </BrowserRouter>
     </ErrorBoundary>
